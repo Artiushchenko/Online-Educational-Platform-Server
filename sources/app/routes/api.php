031db221e7866737d54a938044fe437dc5f5fffb
Artiushchenko <Artyushchenko2018@gmail.com>
2024-11-13 21:12:18 +0200

    Authorization was implemented

--- sources/app/routes/api.php
+++ sources/app/routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Auth::routes();
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
 });
+
+Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'getUser']);
